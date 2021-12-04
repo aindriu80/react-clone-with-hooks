c3cb4755063bc6659923ad0f4ddc28f8aa3a10b8
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2021-12-04 12:04:29 +0000

    Simple counter enumeration

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import { render } from './MyReact.js'
 import Component from './Component.js'
 
+let propCount = 0
+document.getElementById('btn-prop').addEventListener('click', () => {
+  propCount++
+  renderComponent()
+})
+
 function renderComponent() {
-  render(Component, { propCount: 1 }, document.getElementById('root'))
+  render(Component, { propCount }, document.getElementById('root'))
 }
 renderComponent()
