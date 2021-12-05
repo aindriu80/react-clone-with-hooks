d7a0a78155ec78ffaf728eee1036a3d8833557c7
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2021-12-05 11:46:50 +0000

    setState

--- Component.js
+++ Component.js
@@ -4,6 +4,11 @@ export default function Component({ propCount }) {
   const [count, setCount] = useState(0)
   const propCountDoubled = 0
 
+  setTimeout(() => {
+    setCount((currentCount) => currentCount + 1)
+  }, 1000)
+  
+
   return `
 	State: ${count}
 	Prop: ${propCount}
--- MyReact.js
+++ MyReact.js
@@ -1,13 +1,36 @@
 let globalId = 0
+let globalParent
 const componentState = new Map()
+
 export function useState(initialState) {
-  console.log('component', componentState)
-  return [initialState, () => {}]
+  const id = globalId
+  const parent = globalParent
+  globalId++
+  return (() => {
+    console.log('component', componentState.get(parent))
+    const { cache, props, component } = componentState.get(parent)
+    if (cache[id] == null) {
+      cache[id] = {
+        value:
+          typeof initialState === 'function' ? initialState() : initialState,
+      }
+    }
+    const setState = (state) => {
+      if (typeof state === 'function') {
+        cache[id].value = state(cache[id].value)
+      } else {
+        cache[id].value = state
+      }
+      render(component, props, parent)
+    }
+    return [cache[id].value, setState]
+  })()
 }
 
 export function render(component, props, parent) {
   const state = componentState.get(parent) || { cache: [] }
   componentState.set(parent, { ...state, component, props })
+  globalParent = parent
   const output = component(props)
   globalId = 0
   parent.textContent = output
