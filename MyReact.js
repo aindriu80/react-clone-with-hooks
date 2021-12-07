6fc6779015eb28b6f8e16a358f3b619d88fe0f0c
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2021-12-07 15:52:28 +0000

    useMemo

--- MyReact.js
+++ MyReact.js
@@ -54,6 +54,31 @@ export function useEffect(callback, dependencies) {
     }
   })()
 }
+export function useMemo(callback, dependencies) {
+  const id = globalId
+  const parent = globalParent
+  globalId++
+  return (() => {
+    console.log('component', componentState.get(parent))
+    const { cache } = componentState.get(parent)
+    if (cache[id] == null) {
+      cache[id] = { dependencies: undefined }
+    }
+    const dependenciesChanged =
+      dependencies == null ||
+      dependencies.some((dependency, i) => {
+        return (
+          cache[id].dependencies == null ||
+          cache[id].dependencies[i] !== dependency
+        )
+      })
+    if (dependenciesChanged) {
+      cache[id].value = callback()
+      cache[id].dependencies = dependencies
+    }
+    return cache[id].value
+  })()
+}
 
 export function render(component, props, parent) {
   const state = componentState.get(parent) || { cache: [] }
